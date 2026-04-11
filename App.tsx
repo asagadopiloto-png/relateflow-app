722944468e4754dcd2f1b6742e935d5fe65b54c2
asagadopiloto-png <asagadopiloto@gmail.com>
2026-04-11 09:36:54 -0300

    feat: Rename import from geminiService to Testar Análise Segura

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { RelationalStyle, QuizResult } from './types';
 import { QUIZ_QUESTIONS, STYLE_DETAILS } from './constants';
-import { getRelationalAnalysis } from './services/geminiService';
+import { Testar Análise Segura } from './services/geminiService';
 
 // --- Sub-components ---
 
